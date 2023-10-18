fe1546c9978118fc8c98dd6a32a31e684df1deed
Ravi Ranjan <rav594.r@gmail.com>
2023-10-18 23:49:29 +0530

    Express Interaction with Mongoose

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 // console.log(process.env);
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log('DB Connection successful');
+  })
+  .catch(console.log('Problem connecting to the database'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 97,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error:', err);
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
